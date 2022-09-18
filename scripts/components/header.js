7a2bb66e40aaa3200a8ccd0b4851ab37c026ba7a
Nicolas Bertolino <bertolinonicolas@gmail.com>
2022-09-18 16:53:54 +0200

    header script

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,14 +1,9 @@
 function init() {
-  // Get all sections that have an ID defined
   const sections = document.querySelectorAll("section[id]");
 
-  // Add an event listener listening for scroll
   window.addEventListener("scroll", () => {
-
-    // Get current scroll position
     let scrollY = window.pageYOffset;
 
-    // Now we loop through sections to get height, top and ID values for each
     sections.forEach(current => {
       const sectionHeight = current.offsetHeight;
       const sectionTop = current.offsetTop - 0;
@@ -21,7 +16,6 @@ function init() {
       }
     });
   });
-
 }
 
 export default { init }
